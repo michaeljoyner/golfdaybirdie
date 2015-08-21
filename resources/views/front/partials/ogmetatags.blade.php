8a2b8ef1d18a627e9c2bd5104ac84bc6b65a0d23
Michael Joyner <joyner.michael@gmail.com>
2015-08-21 11:54:34 +0800

    correct fb description

--- resources/views/front/partials/ogmetatags.blade.php
+++ resources/views/front/partials/ogmetatags.blade.php
@@ -3,4 +3,4 @@
 <meta name="og:title" content="Golf Ball Branding"/>
 <meta name="og:site_name" content="Golf Ball Branding"/>
 <meta name="og:type" content="Website"/>
-<meta name="og:description" content="Want your own perfect logo on top of the golf balls? That's exactly what we do."/>
+<meta name="og:description" content="Want your own perfect logo on top of the range golf balls? That's exactly what we do."/>
