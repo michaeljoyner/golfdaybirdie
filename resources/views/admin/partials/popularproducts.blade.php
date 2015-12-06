34f9ef2acbf9072c140bcc200aabcf96c50e75bc
Michael Joyner <joyner.michael@gmail.com>
2015-12-06 20:15:54 +0800

    pop products hunting

--- resources/views/admin/partials/popularproducts.blade.php
+++ resources/views/admin/partials/popularproducts.blade.php
@@ -1,16 +1,17 @@
 <div class="popular-products clearfix">
-    <ol class="popular-products-list">
-        @foreach($popularProducts as $index => $product)
-            <li>
-                <span class="popular-position">{{ $index + 1 }}</span>
-                <a href="/admin/products/{{ $product->id }}">
-                    <img src="{{ $product->smallestImageSrc() }}" alt="product image"/>
-                </a>
-            </li>
-        @endforeach
-    </ol>
+    {{--<ol class="popular-products-list">--}}
+        {{--@foreach($popularProducts as $index => $product)--}}
+            {{--<li>--}}
+                {{--<span class="popular-position">{{ $index + 1 }}</span>--}}
+                {{--<a href="/admin/products/{{ $product->id }}">--}}
+                    {{--<img src="{{ $product->smallestImageSrc() }}" alt="product image"/>--}}
+                {{--</a>--}}
+            {{--</li>--}}
+        {{--@endforeach--}}
+    {{--</ol>--}}
 
-    @if($popularProducts->count() < 1)
-        <h3>{{ $emptyMessage }}</h3>
-    @endif
+    {{--@if($popularProducts->count() < 1)--}}
+        {{--<h3>{{ $emptyMessage }}</h3>--}}
+    {{--@endif--}}
+    {!! var_dump($popularProducts) !!}
 </div>
